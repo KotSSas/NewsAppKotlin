420250465e1c9fc75fe41c806d6fda1a4d7cf395
Smug <sashasmug@gmail.com>
2022-04-16 21:26:33 +0300

    Подтянул все новости Украины первая вкладка меню теперь работает

--- app/src/main/java/ui/NewsViewModel.kt
+++ app/src/main/java/ui/NewsViewModel.kt
@@ -1,9 +1,36 @@
 package ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import models.NewsResponse
 import repository.NewsRepository
+import retrofit2.Response
+import util.Resource
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ):ViewModel() {
+    val breakingNews:MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNewsPage = 1
+
+    init {
+        getBreakingNews("ua")
+    }
+
+    fun getBreakingNews(countryCode:String) =viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode,breakingNewsPage)
+        breakingNews.postValue(handlerBreakingNewsresponse(response))
+    }
+
+    private fun handlerBreakingNewsresponse(response: Response<NewsResponse>):Resource<NewsResponse>{
+        if (response.isSuccessful){
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
